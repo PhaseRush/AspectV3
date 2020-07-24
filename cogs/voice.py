5dc31e48828096e80fa49a35b69feeb33ab0f134
phaserush <leozhang888@yahoo.ca>
2020-07-24 01:31:37 -0700

    Propagate voice errors to text channel

--- cogs/voice.py
+++ cogs/voice.py
@@ -13,6 +13,11 @@ class VoiceCog(commands.Cog, name="Voice"):
     async def unmuteall(self, ctx: commands.Context):
         [await member.edit(mute=False) for member in ctx.author.voice.channel.members]
 
+    @muteall.error
+    @unmuteall.error
+    async def sub_error_handler(self, ctx, error):
+        await ctx.send(f"You likely weren't in a voice channel!\n{error}")
+
 
 def setup(bot):
     bot.add_cog(VoiceCog(bot))
