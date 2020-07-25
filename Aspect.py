dd30198464d7d948d17888432181c36a218d847a
phaserush <leozhang888@yahoo.ca>
2020-07-25 03:15:06 -0700

    Revamp cog loading

--- Aspect.py
+++ Aspect.py
@@ -1,10 +1,14 @@
-from discord.ext import commands
+import datetime
+import os
+import traceback
+
 import discord
+from discord.ext import commands
 
 from config import DISCORD_TOKEN
-import datetime
 
-bot = commands.Bot(command_prefix='$', description="actually put something useful here eventually...", activity=discord.Activity(type=discord.ActivityType.watching, name="cat girls"))
+bot = commands.Bot(command_prefix='$', description="actually put something useful here eventually...",
+                   activity=discord.Activity(type=discord.ActivityType.watching, name="cat girls"))
 
 
 @bot.event
@@ -12,12 +16,19 @@ async def on_ready():
     print(f'Logged on as {bot.user.name} id:{bot.user.id} at {datetime.datetime.now()}')
 
 
-extensions = ['cogs.' + cog_name for cog_name in [
-    'meta',
-    'reddit',
-    'voice'
-]]
+def load_cog(ext: str) -> int:
+    try:
+        bot.load_extension(ext)
+        print(f"Successfully loaded extension: {ext}")
+        return 1
+    except Exception:
+        print(f"Failed to load {ext}")
+        traceback.print_exc()
+        return 0
 
-[bot.load_extension(ext) for ext in extensions]
+
+extensions = ['cogs.' + filename[:-3] for filename in os.listdir("./cogs") if filename.endswith(".py")]
+
+print(f"Successfully loaded {sum([load_cog(ext) for ext in extensions])} out of {len(extensions)} extensions")
 
 bot.run(DISCORD_TOKEN)
