210a4148c92112eba70a03b6b5bf0b7b1d2cddd8
phaserush <leozhang888@yahoo.ca>
2020-07-25 03:14:40 -0700

    Add much more detailed Ping information

--- cogs/meta.py
+++ cogs/meta.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import datetime
 
 
 class MetaCog(commands.Cog, name="Meta"):
@@ -6,12 +7,22 @@ class MetaCog(commands.Cog, name="Meta"):
         self.bot = bot
 
     @commands.command()
-    async def ping(self, ctx):
-        await ctx.send("pong uwu")
+    async def ping(self, ctx: commands.Context):
+        gateway_ping_diff: datetime.timedelta = datetime.datetime.utcnow() - ctx.message.created_at
+        current_time = datetime.datetime.utcnow()
+        msg = await ctx.send("ping: api test")
+        api_ping_diff: datetime.timedelta = msg.created_at - current_time
+        gateway_ping = gateway_ping_diff.total_seconds() * 1000
+        api_ping = api_ping_diff.total_seconds() * 1000
+        websocket_latency = ctx.bot.latency * 1000
+        await msg.edit(
+            content=f"```\nWebSocket Latency: {websocket_latency : .3f}ms\n"
+                    f"Gateway Ping: {gateway_ping: .3f}ms\n"
+                    f"Api Ping: {api_ping: .3f}ms\n"
+                    f"Total ping: {(websocket_latency + gateway_ping + api_ping): .3f}ms```")
 
-    @commands.command(aliases=['eval'])
     @commands.is_owner()
-    async def _(self, ctx, expr):
+    async def eval(self, ctx, expr):
         await ctx.send(eval(expr))
 
 
