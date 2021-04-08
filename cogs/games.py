77a629a53263639b09b30c62402cc09dd66eb0d1
phaserush <leozhang888@yahoo.ca>
2021-04-07 19:58:40 -0700

    added random league champions

--- cogs/games.py
+++ cogs/games.py
@@ -2,6 +2,7 @@ import random
 from discord.ext import commands
 from statistics import fmean, pstdev
 from typing import List
+import json
 
 
 def get_stats(rolls: List[int]) -> (float, float, int):
@@ -22,9 +23,19 @@ class Games(commands.Cog, name="Games"):
             second = int(split_query[1])
             numbers = [random.randrange(second) for _ in range(first)]
             mean, stdev, sum = get_stats(numbers)
-            await ctx.send(f'`{" ".join([str(x) for x in numbers])}\nsum: {sum}\taverage: {mean: .2f}\tstdev: {stdev: .2f}`')
+            await ctx.send(
+                f'`{" ".join([str(x) for x in numbers])}\nsum: {sum}\taverage: {mean: .2f}\tstdev: {stdev: .2f}`')
         except Exception:
             await ctx.send("Invalid input! Try something like `3d20`")
+
+    @commands.command(aliases=["randchamps", "champs"])
+    async def random_champ(self, ctx: commands.Context, num_pics: int = 1):
+        with open("./data/league_champions.json", encoding='UTF-8') as f:
+            champion_json = json.load(f)
+        champions = list(champion_json['data'].keys())
+        random.shuffle(champions)
+        newline = "\n"
+        await ctx.send(f'{newline.join(champions[:num_pics])}')
 
 
 def setup(bot):
