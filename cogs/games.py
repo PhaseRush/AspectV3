7009091dce2399da78322824906db57559718e65
phaserush <leozhang888@yahoo.ca>
2021-04-07 19:42:00 -0700

    Added more dice functionality

--- cogs/games.py
+++ cogs/games.py
@@ -1,5 +1,13 @@
 import random
 from discord.ext import commands
+from statistics import fmean, pstdev
+from typing import List
+
+
+def get_stats(rolls: List[int]) -> (float, float, int):
+    mean = fmean(data=rolls)
+    pop_stdev = pstdev(data=rolls, mu=mean)
+    return mean, pop_stdev, sum(rolls)
 
 
 class Games(commands.Cog, name="Games"):
@@ -12,10 +20,11 @@ class Games(commands.Cog, name="Games"):
             split_query = query.split("d")
             first = int(split_query[0])
             second = int(split_query[1])
-            numbers = [str(random.randrange(second)) for _ in range(first)]
-            await ctx.send(" ".join(numbers))
+            numbers = [random.randrange(second) for _ in range(first)]
+            mean, stdev, sum = get_stats(numbers)
+            await ctx.send(f'`{" ".join([str(x) for x in numbers])}\nsum: {sum}\taverage: {mean: .2f}\tstdev: {stdev: .2f}`')
         except Exception:
-            await ctx.send("Invalid input! Try something like `3d5`")
+            await ctx.send("Invalid input! Try something like `3d20`")
 
 
 def setup(bot):
