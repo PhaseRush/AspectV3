2f53a3984bddccb8e366d3080b1f78044acb69c1
PhaseRush <PhaseAnsible@gmail.com>
2022-09-10 13:27:58 -0700

    Chinese translation, use entire sentence for context

--- cogs/autotranslate.py
+++ cogs/autotranslate.py
@@ -21,26 +21,15 @@ class Autotranslate(commands.Cog, name="Autotranslate"):
         if message.guild.id != 773029270863872050:
             return
 
-        msg: str = message.content
-        chinese_blocks = {}
-
-        already_trolled: bool = False
-        for block in re.findall(r'[\u4e00-\u9fff]+', msg):
-            chinese_blocks[block] = self.deepl.translate_text(block, source_lang="ZH", target_lang="EN-GB").text
-            if message.author.id == 969035316008714250:
-                if self.rand.random() < 0.1 and not already_trolled:
-                    await message.channel.send("[I'm a dog woof woof]")
-                    time.sleep(3)
-                    already_trolled = True
-
-        if len(chinese_blocks) == 0:
+        if len(re.findall(u'[\u4e00-\u9fff]+', message.content)) == 0:
             return
 
-        build_msg: str = msg
-        for zh, en in chinese_blocks.items():
-            build_msg = build_msg.replace(zh, f"[{en}]")
+        if message.author.id == 969035316008714250:
+            if self.rand.random() < 0.1:
+                await message.channel.send("[I'm a dog woof woof]")
+                time.sleep(3)
 
-        await message.channel.send(build_msg)
+        await message.channel.send(f"{message.author}: {self.deepl.translate_text(message.content, source_lang='ZH', target_lang='EN-GB').text}")
 
 
 def setup(bot):
