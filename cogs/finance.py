9e733ee5176ceefefcefb70fc06739262cc8ccd9
PhaseRush <PhaseAnsible@gmail.com>
2023-03-04 23:51:26 -0800

    Added some more finance ticker aliases

--- cogs/finance.py
+++ cogs/finance.py
@@ -31,7 +31,10 @@ def format_large_number(num: float) -> str:
 
 substitutions = {
     "xeqt": "xeqt.to",
-    "appl": "aapl"
+    "appl": "aapl",
+    "novideo": "nvda",
+    "jeff": "amzn",
+    "shintel": "intc"
 }
 
 valid_intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"]  # actually, any 'd' works
