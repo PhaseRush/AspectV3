a1842590ceab787681e931a4c762e2606e5b24fe
PhaseRush <PhaseAnsible@gmail.com>
2023-05-31 21:53:21 -0700

    add some aliases for arkk

--- cogs/finance.py
+++ cogs/finance.py
@@ -34,7 +34,9 @@ substitutions = {
     "appl": "aapl",
     "novideo": "nvda",
     "jeff": "amzn",
-    "shintel": "intc"
+    "shintel": "intc",
+    "bark": "arkk",
+    "barkk": "arkk"
 }
 
 valid_intervals = ["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo",
